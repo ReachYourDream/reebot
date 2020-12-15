68b08d5fdabefc76b90805d748ac0bb2045b82ce
ReachYourDream <radityopradana@gmail.com>
2020-12-16 00:22:39 +0700

    First project commit

---
+++ src/main/java/com/reachyourdream/reebot/ReebotSpringApplication.java
@@ -0,0 +1,34 @@
+package com.reachyourdream.reebot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import org.apache.logging.log4j.util.Constants;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReebotSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReebotSpringApplication.class, args);
+        InitBot();
+    }
+
+    private static void InitBot(){
+        String token = System.getenv("BOT_TOKEN");
+//        token = "Nzg4MDkxMDI4MjU0ODgzODUx.X9ec9w.RffRoyuS-Tw3IgMsnhXRV6voRDk";
+        try{
+            JDABuilder.createLight(token)
+                    .addEventListeners(new CoinFlipListener())
+                    .setActivity(Activity.listening("to your commands :)"))
+                    .setStatus(OnlineStatus.ONLINE)
+                    .build();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/test/java/com/reachyourdream/reebot/ReebotSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reachyourdream.reebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReebotSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
